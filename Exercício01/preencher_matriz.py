5efe13df30322a322d5e4c74184e435922e9cea2
MatheusDBS <matheusdebortolisilva@gmail.com>
2025-08-25 22:13:15 -0300

    S

--- Exercício01/preencher_matriz.py
+++ Exercício01/preencher_matriz.py
@@ -8,7 +8,7 @@ def main():
             for j in range(4096):
                 buffer[i][j] = (i + j) % 256
     end_time = time.time()
-    print(f"Tempo de execução (50 preenchimentos): {end_time - start_time:.4f} segundos")
+    print(f"Tempo de execução: {end_time - start_time:.4f} segundos")
 
 if __name__ == "__main__":
     main()
--- Exercício02/preencher_matriz2.py
+++ Exercício02/preencher_matriz2.py
@@ -8,7 +8,7 @@ def main():
             for i in range(4096):
                 buffer[i][j] = (i + j) % 256
     end_time = time.time()
-    print(f"Tempo de execução (colunas depois linhas, 50 preenchimentos): {end_time - start_time:.4f} segundos")
+    print(f"Tempo de execução: {end_time - start_time:.4f} segundos")
 
 if __name__ == "__main__":
     main()
