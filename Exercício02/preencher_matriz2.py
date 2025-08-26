11e1feda79ebc90eb4aac454b571d5106ab905d0
MatheusDBS <matheusdebortolisilva@gmail.com>
2025-08-25 22:07:51 -0300

    All

---
+++ Exercício02/preencher_matriz2.py
@@ -0,0 +1,13 @@
+import time
+
+def main():
+    start_time = time.time()
+    buffer = [[0 for _ in range(4096)] for _ in range(4096)]
+    for j in range(4096):
+        for i in range(4096):
+            buffer[i][j] = (i + j) % 256
+    end_time = time.time()
+    print(f"Tempo de execução (colunas depois linhas): {end_time - start_time:.4f} segundos")
+
+if __name__ == "__main__":
+    main()
